5582fe65e02771625e5b2eef505d416cdb0b4fae
pdp123 <67596821+pdp123@users.noreply.github.com>
2024-09-16 09:00:45 +0000

    second commit

---
+++ 01_basics/datatypeconversion.js
@@ -0,0 +1,44 @@
+// let score =  true
+// // const {test} = req.body
+// // 1- true 0-false
+// // console.log(typeof score)
+
+// let valueInNumber = Number(score)
+
+// console.log(valueInNumber);
+// NaN - Not a number - its a special type
+
+// console.log(typeof valueInNumber)
+
+// "33" => 33
+//"abc"=>NaN not a number
+//true -> 1
+
+
+// let isLoggedIn  = "false"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let num = 33
+
+let stringNumber = String(num)
+// console.log(typeof(stringNumber));
+
+//* Operations
+let value = 3
+let negValue = -value
+// console.log(true+1+false);
+
+let num1,num2,num3 
+
+num1 = num2 = num3 = 2+2
+
+console.log("num1"+" "+num2);
+
+
+
+
+
+
