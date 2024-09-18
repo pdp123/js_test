43745ed3bcc6551dc7af4b27f8bf85a0e6a38582
pdp123 <67596821+pdp123@users.noreply.github.com>
2024-09-18 15:18:57 +0000

    obj2final

---
+++ objects/obj3.js
@@ -0,0 +1,17 @@
+// destrcuture object
+
+const course = {
+    couseName:"js in hindi",
+    price:"999",
+    courseInstructors:"Hitsh"
+}
+
+const {courseInstructors:instructor} = course
+
+console.log(instructor);
+
+const navbar = ({}) => {
+
+}
+
+navbar(compnayName:"Hitesh")
